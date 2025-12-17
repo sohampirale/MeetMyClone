6b23f6061c6609a93226abc8ab78e529f1295400
sohampirale <sohampirale20504@gmail.com>
2025-12-17 16:32:04 +0530

    Add aws strands agent processor into pipecat pipeline

--- backend/testing/pipecat-quickstart/bot.py
+++ backend/testing/pipecat-quickstart/bot.py
@@ -20,7 +20,7 @@ Run the bot using::
 """
 
 import os
-BASE_DIR = Path(__file__).parent
+#BASE_DIR = Path(__file__).parent
 from pipecat.frames.frames import (
     Frame,
     TranscriptionFrame, 
@@ -66,31 +66,130 @@ from pipecat.pipeline.parallel_pipeline import ParallelPipeline
 from pipecat.transports.base_transport import BaseTransport, TransportParams
 from pipecat.transports.daily.transport import DailyParams
 from pipecat.frames.frames import Frame
-from pipecat.frames.frame_direction import FrameDirection  # Correct location
+#from pipecat.frames.frame_direction import FrameDirection  # Correct location
 from pipecat.processors.frame_processor import FrameProcessor
-
+from pipecat.frames.frames import (
+    LLMMessagesFrame,
+    UserStoppedSpeakingFrame,
+    TranscriptionFrame,
+    StartInterruptionFrame,
+    BotInterruptionFrame,
+    LLMContextFrame,
+)
+from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContextFrame
+from pipecat.processors.frameworks.strands_agents import StrandsAgentsProcessor
 
 logger.info("✅ All components loaded successfully!")
 
 load_dotenv(override=True)
 
+from strands import Agent,tool
+from strands.models.litellm import LiteLLMModel
+from strands_tools import calculator # Import the calculator tool
+
+
+@tool
+def smart_background_agents(topic:str):
+    """Important tool for voice agent to run background specialized powerful agents for research
+    
+    Use this tool mid conversation based on users interest and multiple specialized agents will run in background to collect information for you and provide you shortly
+
+    Input : topic: describe what you want the background agents to do 
+    """
+
+    print('------------------INSIDE smart_background_agents----------------')
+    print(f'topic : {topic}')
+    return "background specialized agents have started"
+
+model = LiteLLMModel(
+    client_args={
+        "api_key":os.getenv('OPENROUTER_API_KEY'),
+    },
+    model_id="openrouter/openai/gpt-4o-mini",
+    # model_id="openrouter/google/gemini-2.0-flash-lite-001",
+    # model_id="openrouter/google/gemini-2.0-flash-exp:free",
+    # model_id="openrouter/google/gemini-2.0-flash-001",
+    # model_id="openrouter/google/gemini-2.5-pro",
+    params={
+        'temperature':0.5,
+        "max_tokens":1000
+    },
+
+)
+
+agent = Agent(
+    model=model,
+    tools=[calculator,smart_background_agents],
+    system_prompt="You are a voice ai agent in realtime meeting with user,use tool 'smart_backgroud_agents' who will help you in the backgroud with info and task you delegate to them, DO NOT tell user about this and meantime try to answer by yourself you will know when the answer comes until them you have to handle user without that info and without letting him know that you dont have the information yet, and also in your resposnes to user add fillers like um hm or any more where appropriate to make it sound more humanistic"
+)
+
+
+messages = [
+        {
+            "role": "system",
+            "content": "You are a friendly AI assistant. When the conversation starts say 'Hey there Soham'",
+        }
+]
+
+context = LLMContext(messages)
+context_aggregator = LLMContextAggregatorPair(context)
+
+
+def dump_frame(frame):
+    print(f"\n=== {frame.__class__.__name__} ===")
+    for attr, value in vars(frame).items():
+        print(f"{attr}: {value}")
 
 class AWSStrandsProcessor(FrameProcessor):
     def __init__(self):
         super().__init__()
-        self.transcriptionFrameFound=False
-        self.llmContextFrameFound=False
-        self.llmMessageFrameFound=False    
-
+        self.seen_llm_messages = False
+        self.seen_user_stopped = False
+        self.seen_transcription = False
+        self.seen_start_interruption = False
+        self.seen_stop_interruption = False
+        self.seen_llm_context = False
+        
     async def process_frame(self, frame: Frame, direction):
         await super().process_frame(frame, direction)
 
         await self.push_frame(frame, direction)
-            
-        if isinstance(frame, LLMMessagesFrame) and  self.transcriptionFrameFound==False:
-            self.transcriptionFrameFound=True
-            print(f'LLMMessagesFrame  : {frame}')
-    
+        if isinstance(frame, LLMMessagesFrame) and not self.seen_llm_messages:
+            print("LLMMessagesFrame (LLM INPUT, not trigger)")
+            self.seen_llm_messages = True
+            dump_frame(frame)
+
+
+        elif isinstance(frame, UserStoppedSpeakingFrame) and not self.seen_user_stopped:
+            print("UserStoppedSpeakingFrame (TURN END signal)")
+            self.seen_user_stopped = True
+            dump_frame(frame)
+
+        elif isinstance(frame, TranscriptionFrame) and not self.seen_transcription:
+            print("TranscriptionFrame (PARTIAL / INTERMEDIATE ASR)")
+            self.seen_transcription = True
+            dump_frame(frame)
+
+        elif isinstance(frame, BotInterruptionFrame) and not self.seen_start_interruption:
+            print("BotInterruptionFrame (INTERRUPTION BOT control)")
+            self.seen_start_interruption = True
+            dump_frame(frame)
+
+        elif isinstance(frame, LLMContextFrame) and not self.seen_llm_context:
+            #gives context not for triggering
+            print(f'context at the moment is ')
+            dump_frame(context)
+            dump_frame(frame)
+            #print("LLMContextFrame ",frame)
+            #self.seen_llm_context = True
+
+        elif isinstance(frame,OpenAILLMContextFrame) and not self.seen_llm_context:
+            #gives context not for triggering
+            dump_frame(frame)
+            #print("LLMContextFrame ",frame)
+            #self.seen_llm_context = True
+
+
         
 
 
@@ -107,7 +206,7 @@ async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
     )
     
     aws_strands=AWSStrandsProcessor()
-
+    strands_agent = StrandsAgentsProcessor(agent=agent)
     # llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"))
     llm = GoogleLLMService(
         api_key=os.getenv("GEMINI_API_KEY"),
@@ -116,12 +215,12 @@ async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
     )
 
 
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a friendly AI assistant. When the conversation starts say 'Hey there Soham'",
-        },
-    ]
+    #messages = [
+    #    {
+    #        "role": "system",
+    #        "content": "You are a friendly AI assistant. When the conversation starts say 'Hey there Soham'",
+    #    },
+    #]
 
     context = LLMContext(messages)
     context_aggregator = LLMContextAggregatorPair(context)
@@ -135,6 +234,7 @@ async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
             stt,
             context_aggregator.user(),  # User responses
             aws_strands,       
+            strands_agent,
             tts,  # TTS
             transport.output(),  # Transport bot output
             context_aggregator.assistant(),  # Assistant spoken responses
@@ -180,7 +280,7 @@ async def bot(runner_args: RunnerArguments):
         "webrtc": lambda: TransportParams(
             audio_in_enabled=True,
             audio_out_enabled=True,
-            vad_analyzer=SileroVADAnalyzer(params=VADParams(stop_secs=0.2)),
+            vad_analyzer=SileroVADAnalyzer(params=VADParams(stop_secs=0.4)),
             turn_analyzer=LocalSmartTurnAnalyzerV3(),
         ),
     }
